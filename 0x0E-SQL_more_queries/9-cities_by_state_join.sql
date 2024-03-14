f421b82f0dad3696e639ff04b2b0043206f03d06
Ufoma <dymondintheski@gmail.com>
2024-03-14 04:22:48 +0100

    fix task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,5 @@
 --lists all cities that can be found in a database
 
-SELECT cities.id, cities.name, states.name AS state_name
-FROM cities, states
-WHERE cities.state_id = states.id
-ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities
+LEFT JOIN states ON states.id = cities.state_id
+ORDER BY cities.id;

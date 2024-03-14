f3b9bc4d5395c1586837d9517f0b332faa83fbf5
Ufoma <dymondintheski@gmail.com>
2024-03-14 03:11:15 +0100

    cities of california

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,7 @@
+-- lists all the cities of california
+
+USE hbtn_0d_usa;
+SELECT cities.* FROM cities, states
+WHERE cities.state_id = states.id
+  AND states.name = 'California'
+ORDER BY cities.id ASC;
